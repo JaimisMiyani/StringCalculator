38cfe972158f588c03895b81e6e23328426d5279
JaimisMiyani <jaimismiyani12@gmail.com>
2021-08-29 14:32:54 +0530

    3.Multivalue string with "\n"(Error)

--- StringCalculator/src/StringCalculatorTest.java
+++ StringCalculator/src/StringCalculatorTest.java
@@ -11,22 +11,27 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void isEmptyCheck() {
+    public void isEmpty() {
         Assert.assertEquals(0,calc.Add(""));
     }
 
     @Test
-    public void isSingleValue() {
+    public void isSingle() {
         Assert.assertEquals(1,calc.Add("1"));
     }
 
     @Test
-    public void sumofTwoValue() {
+    public void sumofTwo() {
         Assert.assertEquals(3,calc.Add("1,2"));
     }
 
     @Test
-    public void sumofMultipleValue() {
+    public void sumofMultiple() {
         Assert.assertEquals(15,calc.Add("1,2,3,4,5"));
+    }
+
+    @Test
+    public void sumofMultipleNewline() {
+        Assert.assertEquals(15,calc.Add("1,2,3,\n4,5"));
     }
 }
